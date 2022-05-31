ab6a45122275d4cb81845356379ca46a715e47d5
ASUS <Yehor Kholodiuk>
2022-05-31 16:38:22 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Todo from "./todo";
 function App() {
   return (
     <div>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/todo">To-Do</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+          <Route path="todo" element={<Todo/>}/>
       </Routes>
     </div>
   );
---
+++ src/todo.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+
+const initialList = [
+    {id: 123, name: 'ABC', status: 'todo'},
+    {id: 234, name: 'Learn React', status: 'progress'},
+    {id: 456, name: 'JavaScript', status: 'review'}
+]
+
+function App() {
+    const [taskName, setTaskName] = useState('')
+    const [list, setList] = useState(initialList)
+
+    const addTask = () => {
+        console.log(taskName)
+        setList([...list, {id: Math.random(), name: taskName, status: 'todo'}]);
+        setTaskName('')
+    }
+
+    const deleteTask = (id) => {
+        const newList = list.filter(el => el.id !== id)
+        setList(newList)
+    }
+
+    const changeStatusDone = (id) => {
+        const n = list.map(el => el.id === id ? {...el, status: 'done'} : el)
+        setList(n)
+    }
+
+
+    const selectTaskStatus = (id, event) => {
+        setList(list.map(el => el.id === id ? {...el, status: event.target.value} : el))
+    }
+
+
+    return (
+        <div className="App">
+            <h1>Task:{taskName}</h1>
+            <input value={taskName} onChange={e => setTaskName(e.target.value)}/>
+            <button onClick={addTask}>Add Task</button>
+            <ul>
+                {
+                    list.map(el => <li key={el.id} className={el.status}>{' '}{el.name}<strong>{el.status}{' '}</strong>
+                        <select value={el.status} onChange={event => selectTaskStatus(el.id, event)}>
+                            <option value="todo">todo</option>
+                            <option value="progress">progress</option>
+                            <option value="review">review</option>
+
+
+                        </select>
+                        <button onClick={() => deleteTask(el.id)}>Delete</button>
+                        <button onClick={() => changeStatusDone(el.id)}>Done</button>
+
+                    </li>)
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default App;
