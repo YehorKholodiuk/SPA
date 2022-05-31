4f3429d8ec86dcb700f9eec6b73514b2e22a3103
ASUS <Yehor Kholodiuk>
2022-05-31 17:32:59 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Todo from "./todo";
+import Counter from "./Counter";
+
 function App() {
-  return (
-    <div>
+    return (
+        <div>
 
             <li><Link to="/">Home</Link></li>
-           <li>
-               <Link to="/about">About</Link>
-           </li>
-           <li>
-               <Link to="/todo">To-Do</Link>
-           </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/todo">To-Do</Link>
+            </li>
+            <li>
+                <Link to="/counter">Counter</Link>
+            </li>
 
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-          <Route path="todo" element={<Todo/>}/>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="about" element={<About/>}/>
+                <Route path="todo" element={<Todo/>}/>
+                <Route path="counter" element={<Counter/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    const [count,setCount] = useState(5)
+    const plus = () => {
+        setCount(count + 1)
+    }
+    const minus = () => {
+        setCount(count - 1)
+    }
+    return (
+        <div className="App">
+            <button onClick={minus}>minus</button>
+            {count}
+            <button onClick={plus}>plus</button>
+        </div>
+    );
+}
+
+export default App;
