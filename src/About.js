f578573f96ecab9028d095858a92706d30ca1606
ASUS <Yehor Kholodiuk>
2022-06-03 16:15:41 +0300

    initial commit

--- src/About.js
+++ src/About.js
@@ -2,8 +2,184 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className="App1">
+            <h1>Kholodiuk Yehor</h1>
+            <header>
+                Date of Birth: 03.04.1985.
+                Citizenship: Cyprus (I don’t have any visa issues)
+                Nationality: Ukrainian.
+                Marital status: single.
+                Address: Arch.Makarios Avenue 219, Gloria house, flat 502, Limassol.
+                Home phone: +357 99 077 536
+                Email: egor.kholodiuk@gmail.com
+                Position wanted: front – end developer, JavaScript developer.
+
+            </header>
+            <h2>
+                CORE SKILLS AND EXPERIENCE
+            </h2>
+
+            I am responsible person with professional attitude to work.
+            <p/>
+            Years of experience in different fields of business activity gave me a great foundation of knowledge and
+            active thinking.
+            <p/>
+            Back office experience in corporate governance matters taught me to be precise and mindful.
+            Studying in the multicultural environment helped me to develop the perception of diverse cultures and
+            behaviors and sociability.
+            The increased need for professionals in IT specialties prompted me to master the specialty of front-end
+            developer.
+            I enjoy working with others but equally able to work on my own.
+            <hr/>
+            Employment
+            <ol>
+                <li>
+                    LANARS (Custom software development services in Ukraine, Dnipro)
+                    Front-End Developer (remote position)
+                    August 2021 – November 2021.
+                    Participation in the development and testing of web application. https://byzans.com/ (tool for
+                    readers).
+                </li>
+
+                <li>PRIMECAP CYPRUS LIMITED (Cyprus, Limassol)
+                    Associate, Back Office Specialist.
+                    December 2010 – August 2014
+                    Responsibilities include:
+                    Record–keeping of inside current documents of the Company and management of the documents
+                    circulation.
+                    Support and technical assistance for the project managers of the Company in the arrangement of
+                    documentation of projects.
+                    Storage and data structuring of the electronic documents of the projects.
+                    Corporate advisory on all aspects in relation to restructurings and reorganizations, constitutional
+                    matters, statutory and regulatory compliance, corporate governance matters and shareholder meetings.
+                    Collection of documents required for KYC procedure.
+                </li>
+
+
+                <li>
+                    Private Enterprise “TechTorgBusiness” (Ukraine, Dnepropetrovsk)
+                    Office-manager, Administrator.
+                    April 2008 – September 2010
+                    Responsibilities include:
+                    Establishing and maintaining business relationships with new clients.
+                    Support and technical assistance of the intermediary activity.
+                    Assistance with reconciliation of balance sheet accounts.
+                    Maintenance of the current document circulation.
+                    Analysis of markets of goods.
+
+                </li>
+
+                <li>
+                    Business Invest Investment Company, Ltd
+                    (Ukraine, Dnepropetrovsk)
+                    Trade Specialist
+                    September 2007 - December 2007
+                    Responsibilities include:
+                    Assistance in managing of share acquisition deals.
+                    Preparation of documents for share acquisition deals.
+                    Preparation of internal memo and instruction letters.
+                </li>
+
+            </ol>
+
+
+            <h2>
+                ADDITIONAL SKILLS / ATTRIBUTES
+            </h2>
+
+           <h4>
+               Computer Skills:
+           </h4>
+
+                <ol>
+                    <li>Proficient knowledge of MS Office, Adobe Acrobat X Pro.</li>
+                </ol>
+                <h4>Knowledge of foreign languages:</h4>
+                <li>
+                    Ukrainian – is a native speaker.
+                </li>
+
+                <li>
+                    Russian – is a native speaker.
+                </li>
+
+                <li>
+                    English – fluent verbal and written knowledge.
+                </li>
+
+                <li>
+                    Greek – moderate, continual improvement.
+                </li>
+
+                <li>
+                    German - moderate, continual improvement.
+                </li>
+
+
+
+
+            <h2>
+                EDUCATION:
+            </h2>
+            <ol>
+                <h4>Programming Academy in Silicon Valley (PASV). (Sunnyvale, California)</h4>
+                <li>Certificate for the successful completion of the JavaScript Syntax course. (Completion date - July, 20,
+                    2020) – proficient knowledge.</li>
+
+                <li>Certificate for the successful completion of the JavaScript Practice course.(Completion date – December
+                    1, 2020) – proficient knowledge.</li>
+
+                <li>
+                    Certificate for the successful completion of the Front-End React course.(Completion date – June, 2021) –
+                    proficient knowledge.
+                </li>
+
+                <h5>Other courses that have been undertaken and successfully completed:</h5>
+
+                <li>
+                    1) HTML, CSS Syntax - good working knowledge.
+                </li>
+                <li>
+                    2) Express JS – good working knowledge.
+                </li>
+                <li>3) React Redux – good working knowledge.</li>
+
+                <li>
+                    4) Mongo DB and Postman ( experience with Restful API) – good working knowledge.
+                </li>
+                <section>
+                    I am familiar with Webpack, Docker, Mocha (I came across this test framework, while solving tasks on the
+                    www.codewars.com (educational platform)), and priorities of ES6 (and I like everything about it). I am
+                    familiar with Typescript.
+                    I know principles of SOLID.
+                </section>
+
+
+            </ol>
+
+
+            Additional info:
+            https://github.com/YehorKholodiuk
+            https://www.linkedin.com/in/yehor-kholodiuk-9bbb6238/
+
+             CTL EUROCOLLEGE (Cyprus, Limassol).
+            Master of Business Administration (2012 - 2014).
+
+             Dnepropetrovsk University of Economics and Law, (Ukraine).
+            Bachelor of Science (academic equivalent of the specialist degree) in
+            Finance (2003 – 2007).
+            Subject of the Diploma project: “Evaluation of the financial and economic
+            activity based on the example of the big scale enterprise (industrial
+            Enterprise).
+
+            Driving experience: since 2008.
+            Personal skills:
+            High efficiency, analytical mind, honesty, integrity, sociability, high trainability.
+            Hobbies:
+            Music (rock, medieval, new age), reading, learning foreign languages, karaoke
+            singing, swimming, training at the gym, travelling.
+
+
         </div>
     );
 };
